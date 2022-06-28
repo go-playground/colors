420cc2561a46310386aa29451052863f924b557c
gstvg <28798827+gstvg@users.noreply.github.com>
2022-06-27 23:01:55 -0300

    add doc to interface methods too

--- colors.go
+++ colors.go
@@ -12,13 +12,30 @@ var (
 
 // Color is the base color interface from which all others ascribe to
 type Color interface {
+	// ToHEX converts the Color interface to a concrete HEXColor
 	ToHEX() *HEXColor
+
+	// ToRGB converts the Color interface to a concrete RGBColor
 	ToRGB() *RGBColor
+
+	// ToRGBA converts the Color interface to a concrete RGBAColor
 	ToRGBA() *RGBAColor
+
+	// String returns the string representation of the Color
 	String() string
-	IsLight() bool // http://stackoverflow.com/a/24213274/3158232 and http://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx
-	IsDark() bool  //for perceived luminance, not strict math
+
+	// IsLight returns whether the color is perceived to be a light color
+	// http://stackoverflow.com/a/24213274/3158232 and http://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx
+	IsLight() bool
+
+	// IsDark returns whether the color is perceived to be a dark color
+	//for perceived luminance, not strict math
+	IsDark() bool
+
+	// RGBA implements std-lib color.Color interface
 	RGBA() (r, g, b, a uint32)
+
+	// Equal reports whether the colors are the same
 	Equal(Color) bool
 }
 
